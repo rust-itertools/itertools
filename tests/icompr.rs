b62a6ab7d633f5f4142cc282e62f05540088091e
root <root@localhost>
2014-12-13 02:04:42 +0100

    Add test for icompr!()

---
+++ tests/icompr.rs
@@ -0,0 +1,16 @@
+#![feature(phase)]
+
+#[phase(link, plugin)]
+
+extern crate itertools;
+
+use std::iter::AdditiveIterator;
+
+#[test]
+fn icompr() {
+    let log = "GET / 4096\nGET /home/ 16301\nPOST /home/ 49\nGET / 4096\n";
+    let lines = log.lines();
+    let rows = icompr!(line.words() for line in lines);
+    let ngets = icompr!(1i for mut row in rows if row.next() == Some("GET")).sum();
+    assert_eq!(ngets, 3i);
+}
