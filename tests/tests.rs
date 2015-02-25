6dfc9a5df64d204172fd0cf7407773a6c278f5d4
flo-l <lacknerflo@gmail.com>
2015-02-25 17:01:15 +0100

    add tests for MultiPeek

--- tests/tests.rs
+++ tests/tests.rs
@@ -13,6 +13,7 @@ extern crate test;
 
 use std::fmt::Debug;
 use std::iter::order;
+use std::iter::IntoIterator;
 use itertools::Itertools;
 use itertools::Interleave;
 use itertools::Zip;
@@ -333,4 +334,29 @@ fn to_string_join() {
     assert_eq!(many.iter().to_string_join(", "), "1, 2, 3");
     assert_eq!( one.iter().to_string_join(", "), "1");
     assert_eq!(none.iter().to_string_join(", "), "");
+}
+
+#[test]
+fn multipeek() {
+    let nums = vec![1u8,2,3,4,5];
+
+    let multipeek = nums.iter().map(|&x| x).multipeek();
+    assert_eq!(nums, multipeek.collect());
+
+    let mut multipeek = nums.iter().map(|&x| x).multipeek();
+    assert_eq!(multipeek.peek(), Some(&1));
+    assert_eq!(multipeek.next(), Some(1));
+    assert_eq!(multipeek.peek(), Some(&2));
+    assert_eq!(multipeek.peek(), Some(&3));
+    assert_eq!(multipeek.next(), Some(2));
+    assert_eq!(multipeek.peek(), Some(&3));
+    assert_eq!(multipeek.peek(), Some(&4));
+    assert_eq!(multipeek.peek(), Some(&5));
+    assert_eq!(multipeek.peek(), None);
+    assert_eq!(multipeek.next(), Some(3));
+    assert_eq!(multipeek.next(), Some(4));
+    assert_eq!(multipeek.next(), Some(5));
+    assert_eq!(multipeek.next(), None);
+    assert_eq!(multipeek.peek(), None);
+
 }
