744d11403e2df7972d04857cc0007be7edde1a59
root <root@localhost>
2015-04-02 21:30:20 +0200

    Fix zip length test

--- tests/zip.rs
+++ tests/zip.rs
@@ -16,7 +16,7 @@ use itertools::ZipTrusted;
 #[cfg(feature = "unstable")]
 #[test]
 fn test_zip_longest_size_hint() {
-    let c = 0..;
+    let c = (1..10).cycle();
     let v: &[_] = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let v2 = &[10, 11, 12];
 
