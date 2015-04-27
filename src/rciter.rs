d0c7ef5a5d3e04c70a65cbacb0590df644941039
root <root@localhost>
2015-04-27 17:30:15 +0200

    Update docs

--- src/rciter.rs
+++ src/rciter.rs
@@ -3,7 +3,7 @@ use std::iter::IntoIterator;
 use std::rc::Rc;
 use std::cell::RefCell;
 
-/// A wrapper for `Rc<RefCell<I>>`, that implements the `Iterator` trait.
+/// A wrapper for **Rc\<RefCell\<I\>\>**, that implements the **Iterator** trait.
 pub struct RcIter<I> {
     /// The boxed iterator.
     pub rciter: Rc<RefCell<I>>,
