1e19ccc3438e91b9e1735f4ce361c73da388db03
Steven Allen <steven@stebalien.com>
2015-12-29 15:12:07 -0800

    Add example for itertools::func.

--- src/fn_iter.rs
+++ src/fn_iter.rs
@@ -4,6 +4,27 @@ pub struct FnIter<F>(F);
 /// Iterate over a function.
 ///
 /// The returned Iterator calls the passed function on each call to next.
+///
+/// Example:
+///
+/// ```
+/// // Generate the fibonacci sequence (no recursion).
+/// let fib = itertools::func({
+///     let mut a = 0;
+///     let mut b = 1;
+///     move || {
+///         let ret = a;
+///         let next = a + b;
+///         a = b;
+///         b = next;
+///         Some(ret)
+///     }
+/// });
+///
+/// itertools::assert_equal(fib.take(10), vec![0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+/// ```
+///
+/// 
 pub fn func<T, F>(f: F) -> FnIter<F>
     where F: FnMut() -> Option<T>
 {
