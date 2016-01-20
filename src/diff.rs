5592a3e07520fffabfef21d785723aad981996a2
mitchmindtree <mitchell.nordine@gmail.com>
2016-01-21 01:15:55 +1100

    Make Diff symmetric and adjust diff function accordingly. Add diff_by_ref function that retains original behaviour friendlier for iterator caching.

--- src/diff.rs
+++ src/diff.rs
@@ -14,57 +14,83 @@ use std::iter::FromIterator;
 
 /// A type returned by the [`diff`](./fn.diff.html) function.
 ///
-/// `Diff` represents the way in which the elements (of type `E`) yielded by the iterator `I`
-/// differ to some other iterator yielding borrowed elements of the same type.
-///
-/// `I` is some `Iterator` yielding elements of type `E`.
-pub enum Diff<I>
+/// `Diff` represents the way in which the elements yielded by the iterator `I` differ to some
+/// iterator `J`.
+pub enum Diff<I, J>
     where I: Iterator,
+          J: Iterator,
 {
     /// The index of the first non-matching element along with the iterator's remaining elements
     /// starting with the first mis-matched element.
-    FirstMismatch(usize, PutBack<I>),
-    /// The remaining elements of the iterator.
-    Longer(PutBack<I>),
+    FirstMismatch(usize, PutBack<I>, PutBack<J>),
     /// The total number of elements that were in the iterator.
-    Shorter(usize),
+    Shorter(usize, PutBack<I>),
+    /// The remaining elements of the iterator.
+    Longer(usize, PutBack<J>),
 }
 
-/// Compares every element yielded by both elems and new_elems in lock-step and returns a `Diff`
-/// which describes how `j` differs from `i`.
+/// Compares every element yielded by both `i` and `j` in lock-step and returns a `Diff` which
+/// describes how `j` differs from `i`.
+///
+/// If the number of elements yielded by `j` is less than the number of elements yielded by `i`,
+/// the number of `j` elements yielded will be returned along with `i`'s remaining elements as
+/// `Diff::Shorter`.
+///
+/// If the two elements of a step differ, the index of those elements along with the remaining
+/// elements of both `i` and `j` are returned as `Diff::FirstMismatch`.
+///
+/// If `i` becomes exhausted before `j` becomes exhausted, the number of elements in `i` along with
+/// the remaining `j` elements will be returned as `Diff::Longer`.
+pub fn diff<I, J>(i: I, j: J) -> Option<Diff<I::IntoIter, J::IntoIter>>
+    where I: IntoIterator,
+          J: IntoIterator,
+          I::Item: PartialEq<J::Item>,
+{
+    diff_internal(i, j, |ie, je| ie != je)
+}
+
+/// Similar to [`diff`](./fn.diff), however expects `i` to yield references to its elements.
 ///
 /// This function is useful for caching some iterator `j` in some sequential collection without
 /// requiring `j` to be `Clone` in order to compare it to the collection before determining if the
-/// collection needs to be updated. The returned function returns as soon as a difference is found,
+/// collection needs to be updated. The function returns as soon as a difference is found,
 /// producing a `Diff` that provides the data necessary to update the collection without ever
 /// requiring `J` to be `Clone`. This allows for efficiently caching iterators like `Map` or
 /// `Filter` that do not implement `Clone`.
 ///
-/// If the number of elements yielded by `j` is less than the number of elements yielded by `i`,
-/// the number of `j` elements yielded will be returned as `Diff::Shorter`.
-///
-/// If the two elements of a step differ, the index of those elements along with the remaining
-/// elements of `j` are returned as `Diff::FirstMismatch`.
-///
-/// If `i` becomes exhausted before `j` becomes exhausted, the remaining `j` elements will be
-/// returned as `Diff::Longer`.
-///
 /// See [`copy_on_diff`](./fn.copy_on_diff.html) for an application of `diff`.
-pub fn diff<'a, I, J>(i: I, j: J) -> Option<Diff<J::IntoIter>>
+pub fn diff_by_ref<'a, I, J>(i: I, j: J) -> Option<Diff<I::IntoIter, J::IntoIter>>
     where I: IntoIterator<Item=&'a J::Item>,
           J: IntoIterator,
           J::Item: PartialEq + 'a,
 {
+    diff_internal(i, j, |ie, je| *ie != je)
+}
+
+// Compares every element yielded by both `i` and `j`  with the given `is_diff` function in
+// lock-step.
+//
+// Returns a `Diff` which describes how `j` differs from `i`.
+fn diff_internal<I, J, F>(i: I, j: J, is_diff: F) -> Option<Diff<I::IntoIter, J::IntoIter>>
+    where I: IntoIterator,
+          J: IntoIterator,
+          F: Fn(&I::Item, &J::Item) -> bool,
+{
+    let mut i = i.into_iter();
     let mut j = j.into_iter();
-    for (idx, i_elem) in i.into_iter().enumerate() {
+    let mut idx = 0;
+    while let Some(i_elem) = i.next() {
         match j.next() {
-            None => return Some(Diff::Shorter(idx)),
-            Some(j_elem) => if *i_elem != j_elem {
-                return Some(Diff::FirstMismatch(idx, PutBack::value(j_elem, j)));
+            None => return Some(Diff::Shorter(idx, PutBack::value(i_elem, i))),
+            Some(j_elem) => if is_diff(&i_elem, &j_elem) {
+                let remaining_i = PutBack::value(i_elem, i);
+                let remaining_j = PutBack::value(j_elem, j);
+                return Some(Diff::FirstMismatch(idx, remaining_i, remaining_j));
             },
         }
+        idx += 1;
     }
-    j.next().map(|elem| Diff::Longer(PutBack::value(elem, j)))
+    j.next().map(|j_elem| Diff::Longer(idx, PutBack::value(j_elem, j)))
 }
 
 /// Returns `Cow::Borrowed` `collection` if `collection` contains the same elements as yielded by
@@ -113,13 +139,13 @@ pub fn copy_on_diff<'a, C, U, T: 'a>(collection: &'a C, update: U) -> Cow<'a, C>
           T: Clone + PartialEq,
 {
     let c_iter = collection.into_iter();
-    match diff(c_iter.clone(), update.into_iter()) {
+    match diff_by_ref(c_iter.clone(), update.into_iter()) {
         Some(diff) => match diff {
-            Diff::FirstMismatch(idx, mismatch) =>
+            Diff::FirstMismatch(idx, _, mismatch) =>
                 Cow::Owned(c_iter.take(idx).cloned().chain(mismatch).collect()),
-            Diff::Longer(remaining) =>
+            Diff::Longer(_, remaining) =>
                 Cow::Owned(c_iter.cloned().chain(remaining).collect()),
-            Diff::Shorter(num_update) =>
+            Diff::Shorter(num_update, _) =>
                 Cow::Owned(c_iter.cloned().take(num_update).collect()),
         },
         None => Cow::Borrowed(collection),
