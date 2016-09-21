070e7864a862c602e624c4de33b9d3e5d1c79276
bluss <bluss@users.noreply.github.com>
2016-09-21 13:45:04 +0200

    Remove helper trait GenericRange

--- src/misc.rs
+++ src/misc.rs
@@ -97,45 +97,6 @@ impl<X, T, I> DoubleEndedIterator for FlatTuples<I>
     }
 }
 
-/// `GenericRange` is implemented by Rust's built-in range types, produced
-/// by range syntax like `a..`, `..b` or `c..d`.
-pub trait GenericRange {
-    #[doc(hidden)]
-    /// Start index (inclusive)
-    fn start(&self) -> Option<usize> {
-        None
-    }
-    #[doc(hidden)]
-    /// End index (exclusive)
-    fn end(&self) -> Option<usize> {
-        None
-    }
-}
-
-
-impl GenericRange for RangeFull {}
-
-impl GenericRange for RangeFrom<usize> {
-    fn start(&self) -> Option<usize> {
-        Some(self.start)
-    }
-}
-
-impl GenericRange for RangeTo<usize> {
-    fn end(&self) -> Option<usize> {
-        Some(self.end)
-    }
-}
-
-impl GenericRange for Range<usize> {
-    fn start(&self) -> Option<usize> {
-        Some(self.start)
-    }
-    fn end(&self) -> Option<usize> {
-        Some(self.end)
-    }
-}
-
 /// A trait for items that can *maybe* be joined together.
 pub trait MendSlice
 {
