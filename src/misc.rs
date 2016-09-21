8637f915074fec9e02983b2a8592fd66c19cfe36
bluss <bluss@users.noreply.github.com>
2016-09-21 20:24:34 +0200

    Remove the AppendTuple trait (just use multiple FlatTuples impls)

--- src/misc.rs
+++ src/misc.rs
@@ -12,79 +12,65 @@ pub trait IntoIteratorTuple
     fn into_iterator_tuple(self) -> Self::Output;
 }
 
-/// A helper trait for (x, y, z) ++ w => (x, y, z, w),
-/// used for implementing `iproduct!`.
-pub trait AppendTuple<X> {
-    /// Resulting tuple type
-    type Result;
-    /// “Append” value `x` to a tuple.
-    fn append(self, x: X) -> Self::Result;
-}
-
-macro_rules! impl_append_tuple(
-    () => (
-        impl<T> AppendTuple<T> for () {
-            type Result = (T, );
-            fn append(self, x: T) -> (T, ) {
-                (x, )
-            }
-        }
-    );
+macro_rules! impl_cons_iter(
+    () => ();
 
     ($A:ident, $($B:ident,)*) => (
-        impl_append_tuple!($($B,)*);
+        impl_cons_iter!($($B,)*);
         #[allow(non_snake_case)]
-        impl<$A, $($B,)* T> AppendTuple<T> for ($A, $($B),*) {
-            type Result = ($A, $($B, )* T);
-            fn append(self, x: T) -> ($A, $($B,)* T) {
-                let ($A, $($B),*) = self;
-                ($A, $($B,)* x)
+        impl<X, Iter, $($B),*> Iterator for FlatTuples<Iter, (($($B,)*), X)>
+            where Iter: Iterator<Item = (($($B,)*), X)>,
+        {
+            type Item = ($($B,)* X, );
+            fn next(&mut self) -> Option<Self::Item> {
+                self.iter.next().map(|(($($B,)*), x)| ($($B,)* x, ))
+            }
+
+            fn size_hint(&self) -> (usize, Option<usize>) {
+                self.iter.size_hint()
             }
         }
+
+        #[allow(non_snake_case)]
+        impl<X, Iter, $($B),*> DoubleEndedIterator for FlatTuples<Iter, (($($B,)*), X)>
+            where Iter: DoubleEndedIterator<Item = (($($B,)*), X)>,
+        {
+            fn next_back(&mut self) -> Option<Self::Item> {
+                self.iter.next().map(|(($($B,)*), x)| ($($B,)* x, ))
+            }
+        }
+
     );
 );
 
-impl_append_tuple!(A, B, C, D, E, F, G, H, I, J, K, L,);
+impl_cons_iter!(A, B, C, D, E, F, G, H, I, J, K, L,);
 
 /// A helper iterator that maps an iterator of tuples like
 /// `((A, B), C)` to an iterator of `(A, B, C)`.
 ///
 /// Used by the `iproduct!()` macro.
-#[derive(Clone)]
-pub struct FlatTuples<I> {
+pub struct FlatTuples<I, J>
+    where I: Iterator<Item=J>,
+{
     iter: I,
 }
 
-impl<I> FlatTuples<I> {
+impl<I, J> Clone for FlatTuples<I, J>
+    where I: Clone + Iterator<Item=J>,
+{
+    fn clone(&self) -> Self {
+        FlatTuples {
+            iter: self.iter.clone(),
+        }
+    }
+}
+
+impl<I, J> FlatTuples<I, J>
+    where I: Iterator<Item=J>
+{
     /// Create a new `FlatTuples`.
     #[doc(hidden)]
     pub fn new(iter: I) -> Self {
         FlatTuples { iter: iter }
     }
 }
-
-impl<X, T, I> Iterator for FlatTuples<I>
-    where I: Iterator<Item = (T, X)>,
-          T: AppendTuple<X>
-{
-    type Item = T::Result;
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(|(t, x)| t.append(x))
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
-}
-
-impl<X, T, I> DoubleEndedIterator for FlatTuples<I>
-    where I: DoubleEndedIterator<Item = (T, X)>,
-          T: AppendTuple<X>
-{
-    #[inline]
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.iter.next_back().map(|(t, x)| t.append(x))
-    }
-}
-
