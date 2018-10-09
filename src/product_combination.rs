23783818b1d44f3cb888409be32c6b8226778a73
Ethan Lam <elmemphis2000@gmail.com>
2018-10-09 05:29:35 -0500

    Improved Performance

--- src/product_combination.rs
+++ src/product_combination.rs
@@ -3,7 +3,8 @@
 /// See [`.product_combination()`](../trait.Itertools.html#method.product_combination) for more information.
 pub struct ProductCombination<I: Iterator + Clone> {
     data: I,
-    internal_state: Vec<usize>
+    //Internal state is a vec of tuples containing an iterator and the last retrieved item from this iterator
+    internal_state: Vec<(I, I::Item)>
 }
 
 /// Create a new `ProductionCombination` from a clonable iterator.
@@ -17,8 +18,6 @@ pub fn product_combination<I>(iter: I) -> ProductCombination<I>
 }
 
 impl<I: Iterator + Clone> ProductCombination<I> {
-    //Product combinations are formed from a number which has radix data.len() where each digit is the index of an element.
-    //Incrementing this base data.len() number will produce all possible product combinations.
     fn increment(&mut self) {
         let mut pointer: usize = 0;
         let mut tack_on = false;
@@ -28,40 +27,52 @@ impl<I: Iterator + Clone> ProductCombination<I> {
                     tack_on = true;
                     break;
                 },
-                Some(n) => {
-                    *n += 1;
-
-                    match self.data.clone().nth(*n) {
-                        Some(_) => {},
+                Some((iter, item)) => {
+                    let new_item = iter.next();
+                    match new_item {
+                        //Reached the end of the iterator ... restart the iterator and advance the pointer
                         None => {
-                            //No such item exists ... start back at the beginning
-                            *n = 0;
+                            *iter = self.data.clone();
+                            match iter.next() {
+                                None => {
+                                    panic!("0 sized iterator");
+                                },
+                                Some(i) => {
+                                    *item = i;
+                                }
+                            }
+                            pointer += 1;
+                        },
+                        Some(i) => {
+                            *item = i;
+                            break;
                         }
-                    }
-
-                    if *n == 0 {
-                        pointer += 1;
-                    } else {
-                        break;
                     }
                 },
             }
         }
         if tack_on {
-            self.internal_state.push(0);
+            let mut new_iter = self.data.clone();
+            match new_iter.next() {
+                None => {
+                    panic!("0 sized iterator");
+                },
+                Some(new_item) => {
+                    self.internal_state.push((new_iter, new_item));
+                }
+            }
         }
     }
 }
 
 impl<I> Iterator for ProductCombination<I>
-    where I: Iterator + Clone
+    where I: Iterator + Clone, I::Item: Clone
 {
     type Item = Vec<I::Item>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.increment();
 
-        //Map each digit from the base data.len() to its corresponding element
-        Some(self.internal_state.iter().map(|idx| self.data.clone().nth(*idx).unwrap()).collect())
+        Some(self.internal_state.iter().map(|(iter,item)| item.clone()).collect())
     }
 }
