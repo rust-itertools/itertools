4f2364a83845f51a04738774c7ecf7741f194ac6
Diogo Sousa <diogogsousa@gmail.com>
2020-01-08 23:19:54 +0000

    Updated dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@
 //!
 //! ## Rust Version
 //!
-//! This version of itertools requires Rust 1.27 or later.
+//! This version of itertools requires Rust 1.32 or later.
 //!
 //! [`Iterator`]: https://doc.rust-lang.org/std/iter/trait.Iterator.html
 #![doc(html_root_url="https://docs.rs/itertools/0.8/")]
