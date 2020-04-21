a1eadecd94e7d5aba271d9de317f9bdedc2da3cf
Griffin O'Neill <gsoneill1003@gmail.com>
2020-04-21 13:06:26 -0400

    Added tests for zip_with

--- tests/zip.rs
+++ tests/zip.rs
@@ -61,3 +61,40 @@ fn zip_eq_panic2()
     zip_eq(&a, &b).count();
 }
 
+#[test]
+fn zip_with_maxes() {
+    let a = [1, 2, 3, 4, 5, -1, 100];
+    let b = [7, -3, 4, 10, -2, 10, 90];
+    let maxes = a.iter().zip_with(b.iter(), std::cmp::max);
+    itertools::assert_equal(maxes, &[7, 2, 4, 10, 5, 10, 100]);
+}
+
+#[test]
+fn zip_with_short1() {
+    let a = [4, 5];
+    let b = [3, 7, 9];
+    let sums = a.iter().zip_with(b.iter(), |x,y| x + y);
+    itertools::assert_equal(sums, vec![7, 12]);
+}
+
+#[test]
+fn zip_with_short2() {
+    let a = [4, 5, 7, 11];
+    let b = [1];
+    let sums = a.iter().zip_with(b.iter(), |x,y| x - y);
+    itertools::assert_equal(sums, vec![3]);
+}
+
+#[test]
+fn zip_with_size_hint1() {
+    let a = [1, 2, 3];
+    let b = [1, 2, 3, 5, 7, 9];
+    assert_eq!(a.iter().zip_with(b.iter(), std::cmp::min).size_hint(), (3, Some(3)));
+}
+
+#[test]
+fn zip_with_size_hint2() {
+    let a = [-2, 5, 6, 7, 8, 9];
+    let b = [-1];
+    assert_eq!(a.iter().zip_with(b.iter(), std::cmp::min).size_hint(), (1, Some(1)));
+}
