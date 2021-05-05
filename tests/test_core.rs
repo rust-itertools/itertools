9cfca9dda18eabfdc91cc11a261f9ac221dd035c
Alex Touchet <alextouchet@outlook.com>
2021-05-05 09:32:08 -0700

    Update some links and some spelling/capitalization fixes

--- tests/test_core.rs
+++ tests/test_core.rs
@@ -1,6 +1,6 @@
 //! Licensed under the Apache License, Version 2.0
-//! http://www.apache.org/licenses/LICENSE-2.0 or the MIT license
-//! http://opensource.org/licenses/MIT, at your
+//! https://www.apache.org/licenses/LICENSE-2.0 or the MIT license
+//! https://opensource.org/licenses/MIT, at your
 //! option. This file may not be copied, modified, or distributed
 //! except according to those terms.
 #![no_std]
