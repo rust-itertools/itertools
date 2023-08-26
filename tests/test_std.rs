a163451e2af50d95d95eb8a93e8a31c8e530a230
Philippe-Cholet <phcholet@orange.fr>
2023-08-26 09:50:18 +0200

    Test `combinations_with_replacement` counts
    For a fast test: `n` and `k` are only up to 7.

--- tests/test_std.rs
+++ tests/test_std.rs
@@ -1020,6 +1020,28 @@ fn combinations_with_replacement() {
 }
 
 #[test]
+fn combinations_with_replacement_range_count() {
+    for n in 0..=7 {
+        for k in 0..=7 {
+            let len = binomial(usize::saturating_sub(n + k, 1), k);
+            let mut it = (0..n).combinations_with_replacement(k);
+            assert_eq!(len, it.clone().count());
+            assert_eq!(len, it.size_hint().0);
+            assert_eq!(Some(len), it.size_hint().1);
+            for count in (0..len).rev() {
+                let elem = it.next();
+                assert!(elem.is_some());
+                assert_eq!(count, it.clone().count());
+                assert_eq!(count, it.size_hint().0);
+                assert_eq!(Some(count), it.size_hint().1);
+            }
+            let should_be_none = it.next();
+            assert!(should_be_none.is_none());
+        }
+    }
+}
+
+#[test]
 fn powerset() {
     it::assert_equal((0..0).powerset(), vec![vec![]]);
     it::assert_equal((0..1).powerset(), vec![vec![], vec![0]]);
