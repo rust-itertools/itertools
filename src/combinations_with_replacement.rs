79a1b1572bbf5c24498a00e377be775bd1b83801
Philippe-Cholet <phcholet@orange.fr>
2023-09-06 09:10:14 +0200

    Do not sum options
    A mistake on my part.
    It only returns None if the iterator yields None. But it would overflow when an addition does.
    I guess a `checked_sum` method would be nice.
    
    PS: Do not product options either, as multiplications could overflow the same way. That's how I found out.

--- src/combinations_with_replacement.rs
+++ src/combinations_with_replacement.rs
@@ -157,9 +157,6 @@ fn remaining_for(n: usize, first: bool, indices: &[usize]) -> Option<usize> {
         indices
             .iter()
             .enumerate()
-            // TODO: Once the MSRV hits 1.37.0, we can sum options instead:
-            // .map(|(i, n0)| count(n - 1 - *n0, k - i))
-            // .sum()
             .fold(Some(0), |sum, (i, n0)| {
                 sum.and_then(|s| s.checked_add(count(n - 1 - *n0, k - i)?))
             })
