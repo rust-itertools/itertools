2cf7a1c1afebf219a8653dfd259bc1f74f08ed8f
Philippe-Cholet <phcholet@orange.fr>
2023-11-09 19:33:00 +0100

    `Multipeek::fold`

--- src/multipeek_impl.rs
+++ src/multipeek_impl.rs
@@ -102,6 +102,14 @@ where
     fn size_hint(&self) -> (usize, Option<usize>) {
         size_hint::add_scalar(self.iter.size_hint(), self.buf.len())
     }
+
+    fn fold<B, F>(self, mut init: B, mut f: F) -> B
+    where
+        F: FnMut(B, Self::Item) -> B,
+    {
+        init = self.buf.into_iter().fold(init, &mut f);
+        self.iter.fold(init, f)
+    }
 }
 
 // Same size
