3589780fd95a01057eddb38757e8ac45a01a4ad9
Philippe-Cholet <phcholet@orange.fr>
2023-11-26 14:07:00 +0100

    `PadUsing::rfold`

--- src/pad_tail.rs
+++ src/pad_tail.rs
@@ -97,6 +97,16 @@ where
             Some((self.filler)(self.min))
         }
     }
+
+    fn rfold<B, G>(self, mut init: B, mut f: G) -> B
+    where
+        G: FnMut(B, Self::Item) -> B,
+    {
+        init = (self.iter.len()..self.min)
+            .map(self.filler)
+            .rfold(init, &mut f);
+        self.iter.rfold(init, f)
+    }
 }
 
 impl<I, F> ExactSizeIterator for PadUsing<I, F>
