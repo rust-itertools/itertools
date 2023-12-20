f6c65ea0de06ce71c6b04333735024e44b058e99
Adam Nemecek <adamnemecek@gmail.com>
2023-12-20 13:02:13 -0800

    nitpicks

--- src/size_hint.rs
+++ src/size_hint.rs
@@ -60,12 +60,9 @@ pub fn max(a: SizeHint, b: SizeHint) -> SizeHint {
     let (a_lower, a_upper) = a;
     let (b_lower, b_upper) = b;
 
-    let lower = cmp::max(a_lower, b_lower);
+    let lower = a_lower.max(b_lower);
 
-    let upper = match (a_upper, b_upper) {
-        (Some(x), Some(y)) => Some(cmp::max(x, y)),
-        _ => None,
-    };
+    let upper = a_upper.zip(b_upper).map(|(x, y)| x.max(y));
 
     (lower, upper)
 }
@@ -76,11 +73,11 @@ pub fn min(a: SizeHint, b: SizeHint) -> SizeHint {
     let (a_lower, a_upper) = a;
     let (b_lower, b_upper) = b;
     let lower = cmp::min(a_lower, b_lower);
-    let upper =
-        match (a_upper, b_upper) {
-            (Some(u1), Some(u2)) => Some(cmp::min(u1, u2)),
-            _ => a_upper.or(b_upper),
-        };
+
+    let upper = a_upper
+        .zip(b_upper)
+        .map(|(x, y)| x.min(y))
+        .or_else(|| a_upper.or(b_upper));
     (lower, upper)
 }
 
